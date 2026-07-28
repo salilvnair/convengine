0af6875f9349180aeaaf5fe24cd750009496ae7f
salilvnair <salilvnairgithub@gmail.com>
2026-07-27 23:23:39 -0500

    Remove Builder Studio (extracted to convengine-builder-studio)
    React Flow-backed workspace/team/agent-pool/skill/workflow persistence
    and its LLM runtime switching are now published as a standalone module
    (com.github.salilvnair:convengine-builder-studio) so ck8t consumers
    don't have to pull in the old React Flow builder UI backend.

--- src/main/java/com/github/salilvnair/convengine/builder/repo/BsLlmConfigRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.salilvnair.convengine.builder.repo;
-
-import com.github.salilvnair.convengine.builder.entity.CeBsLlmConfig;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BsLlmConfigRepository extends JpaRepository<CeBsLlmConfig, String> {
-}
--- src/main/java/com/github/salilvnair/convengine/builder/repo/BsWorkspaceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.salilvnair.convengine.builder.repo;
-
-import com.github.salilvnair.convengine.builder.entity.CeBsWorkspace;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BsWorkspaceRepository extends JpaRepository<CeBsWorkspace, String> {
-}
